e8d448b49194a664cf3aa55f94819540e6e57222
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-18 14:58:39 +0900

    足します！

---
+++ 1-4/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[]args){
+      
+    double pi=3.14;
+    int pie=5;
+    System.out.println("半径＋pie+cmの」パイの面積は、");
+    pi=10;
+    System.out.println("半径"+pie+"cmのパイの面積は、");
+    System.out.println(pie*pie*pi);
+    System.out.println("パイの半径を倍にします");
+    pi=10;
+    System.out.println(pie*pie*pi);
+    
+    
+    }
+}
