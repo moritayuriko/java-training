fe273edcddf8519e1cb2dd6c8b8bffcae8617cba
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-18 17:08:30 +0900

    ４

---
+++ 4-16/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean judge = true;
+        if (judge == true) {
+            int[] array = { 1, 2, 3 };
+        }
+    }
+
+}
