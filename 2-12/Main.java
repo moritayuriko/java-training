34170a73ef84a2ba384f90943387f5bd3af16ab4
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-18 15:55:31 +0900

    とくになし

---
+++ 2-12/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "すがわら";
+        String meseage;
+        System.out.print("私の名前は");
+        System.out.print(name);
+        System.out.print("です");
+
+    }
+}
