d905d5b46b0ef8c245cb10a31e6aafd267d93509
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-22 11:00:33 +0900

    9-1~9-4

---
+++ 9-1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[]args){
+        Hero h;
+        h= new Hero();
+        h.hp=100;
+
+    }
+    
+}
---
+++ study1-1/ex-8-3/Cleric copy 2.java
@@ -0,0 +1,16 @@
+public class Cleric {
+    String name;
+    int hp=50;
+    final int MaxHP=50;
+    int mp=10;
+    final int MaxMP=10;
+
+    public void selfAid(){
+    this.hp+=MaxHP;
+    this.mp-=5;
+    System.out.println(this.name+"は、セルフエイドを使った！");
+    System.out.println(this.name + "のMPは" + 5 + "減った!");
+    System.out.println(this.name+"のHPは最大になった!");
+
+}
+}
