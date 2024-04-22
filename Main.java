1d819f611e7a81d45b7e1ed8a50f4258b271bb31
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-22 17:55:17 +0900

    練習問題9-1まで、と9-1の途中

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void heal(int hp) {
+        //ヒールはhpを10回復します
+        hp+=10;
+        
+    }
+    public static void heal(Thief thief){
+        //HPを10回復する
+        thief.hp+=10;
+    }
+    public static void main(String[] args) {
+        //ベースHPは25です。
+        int baseHp = 25;
+        Thief t= new Thief("アサカ",baseHp);
+        //呼び出されたのは2個目の方
+        //ここでどっちも25を得ている？
+        heal(baseHp);
+       //↑が動いてない！っぽい。
+        System.out.println(baseHp+":"+t.hp);
+        //25:25
+        heal(t);
+        System.out.println(baseHp+":"+t.hp);
+        //25:35
+       
+    }
+}
+
--- study1-1/ex-8-4/Cleric.java
+++ study1-1/ex-8-4/Cleric.java
@@ -1,47 +1,52 @@
 
-    public class Cleric {
-        String name;
-        int hp = 50;
-        final int MaxHP = 50;
-        int mp = 10;
-        final int MaxMP = 10;
+public class Cleric {
+    String name;
+    int hp = 50;
+    final int MaxHP = 50;
+    int mp = 10;
+    final int MaxMP = 10;
 
-        public void selfAid() {
-            this.hp = MaxHP;
-            this.mp -= 5;
-            System.out.println(this.name + "は、セルフエイドを使った！");
-            System.out.println(this.name + "のMPは" + 5 + "減った!");
-            System.out.println(this.name + "のHPは最大になった!");
+    public void selfAid() {
+        this.hp = MaxHP;
+        this.mp -= 5;
+        System.out.println(this.name + "は、セルフエイドを使った！");
+        System.out.println(this.name + "のMPは" + 5 + "減った!");
+        System.out.println(this.name + "のHPは最大になった!");
+    }
+
+    public int pray(int byou) {
+        System.out.println(this.name + "は祈り始めた!");
+        if (byou == 0) {
+            System.out.println("祈りが中断され、何も起こらなかった…");
+            return 0;
+        }
+        int lucky = new java.util.Random().nextInt(3) + byou;
+        int mpkaihukuryou = this.mp + lucky;
+        if (mpkaihukuryou > MaxMP) {
+            //仮に今の値（今のMP）がわからなくても、将来的には他の呼び出し先とかでthis mpの値は何かしら入るから、
+            //！今値が入っていなくても！式としては成立するよ。
+            lucky=MaxMP-this.mp;
+            this.mp = MaxMP;
+
+        } else {
+            this.mp += lucky;
         }
 
-        public int pray(int byou) {
-            System.out.println(this.name+"は祈り始めた!");
-            if(byou==0){
-                System.out.println("祈りが中断され、何も起こらなかった…");
-                return 0;
-            }
-            int lucky = new java.util.Random().nextInt(3) + byou;
-            int mpkaihukuryou=this.mp + lucky;
-        if(mpkaihukuryou>MaxMP){
-            this.mp=MaxMP;
-           }else{
-            this.mp+=lucky;
-           }
-
-            if(byou!=0){
-System.out.println(this.name + "は" + byou + "秒間祈って" + "MPを"+this.mp + "に回復した！");
-            }
-            return lucky;
+        if (byou != 0) {
+            System.out.println(this.name + "は" + byou + "秒間祈って" + "MPを" + this.mp + "に回復した！");
         }
-        public static void main(String[] args) {
-            //確認でヒーラーさんを生成
-            Cleric healer = new Cleric();
-            healer.name = "ハピナス";
-            
-            healer.selfAid();
-            healer.pray(0);
-           
+        //このリターンラッキーは実際に回復した量を示しているよ。
+        //元のmp６+祈って得た値４＝１０でいうと４が欲しいっていうリターン
+        return lucky;
+    }
+
+    public static void main(String[] args) {
+        // 確認でヒーラーさんを生成
+        Cleric healer = new Cleric();
+        healer.name = "ハピナス";
+
+        healer.selfAid();
+        healer.pray(3);
 
     }
 }
-
