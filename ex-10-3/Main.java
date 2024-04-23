f2de9514cebd5f0896063311ef2594df96780d86
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-23 17:24:37 +0900

    保存し忘れ

--- ex-10-3/Main.java
+++ ex-10-3/Main.java
@@ -1,11 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        Hero h = new Hero();
-    PoisonMatango pm = new PoisonMatango('A');
+// public class Main {
+//     public static void main(String[] args) {
+//         Hero h = new Hero();
+//     PoisonMatango pm = new PoisonMatango('A');
     
-            pm.attack(h);
+//             pm.attack(h);
 
         
-        }
-    }
+//         }
+//     }
     
