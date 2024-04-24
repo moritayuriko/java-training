5b64a3c98dc083d47739020daee67203e1b02a33
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-24 16:20:27 +0900

    フォルダ整理

--- study1-1/ex-11-4/TangibleAsset.java
+++ study1-1/ex-11-4/TangibleAsset.java
@@ -2,8 +2,8 @@ public abstract class TangibleAsset implements Asset,Thing{
     String name;
     String price;
     String color;
-    double weight;
-    
+    private double weight;
+
     public void tokutyou (){
         this.name=name;
         this.price=price;
@@ -13,6 +13,11 @@ public abstract class TangibleAsset implements Asset,Thing{
     public String getName(){return this.name;}
     public String getPrice(){return this.price;}
     public String getColor(){return this.color;}
-
+public double getweight(){
+    return this.weight;
+}
+public void setweight(double weight){
+    this.weight= weight;
+}
 }
 
