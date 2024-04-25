6850dc397a57d4b2c2d1306dcb3027052d2bd43d
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-25 09:58:39 +0900

    練習問題12-1

---
+++ ex-12-1/12-1.java
@@ -0,0 +1,14 @@
+public class 12-1 {
+    <練習問題12-1>
+    (1)「Sword」インスタンス
+    <<「Item」>>
+    「Sword」を生成したが、ザックリと
+    「Item」とみなす。
+    (2)「Monster」
+    「Slime」
+    「Slime」インスタンス
+    Slimeを生成したが、ザックリと
+    「Monster」とみなす。
+    }
+    
+    
