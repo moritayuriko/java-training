4379d718b4106d722a56ea140357263c84728e0c
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-25 11:03:55 +0900

    練習12-2まで

---
+++ A.java
@@ -0,0 +1,9 @@
+public final class A extends Y {
+    //finalはAクラスは継承の素材にできませんよってこと
+    //内部では呼べる！
+    public void a(){System.out.println("Aa");}
+    public void b(){System.out.println("Ab");}
+    public void c(){System.out.println("Ac");}
+    
+    
+}
---
+++ ex-12-2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Y y1 = new A();
+        Y y2 = new B();
+        y1.a();
+        y2.a();
+  
+    }
+}
