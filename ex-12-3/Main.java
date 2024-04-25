e8eabc205870303f9f760c1471bc94273706f7db
yuriko <yuriko-morita@infinityloop.co.jp>
2024-04-25 11:49:27 +0900

    12-3

---
+++ ex-12-3/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Y[] z = new Y[2];
+        z[0] = new A();
+        z[1] = new B();
+        for (Y roop : z) {
+        // for(int i=0;i<b.length;i++){
+           roop.b();
+
+        }
+    }
+
+    }
