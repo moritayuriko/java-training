e2551569232d23d05d0358b6fa6b50a8234cd77d
yuriko <yuriko-morita@infinityloop.co.jp>
2024-05-07 13:41:58 +0900

    ex-17-4

--- ex-17-4/Main.java
+++ ex-17-4/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        int number = 0;
 
-        if (number < 1) {
-            throw new IOException("数字は1以上にしてください");
+            throw new IOException("");
         }
     }
-}
